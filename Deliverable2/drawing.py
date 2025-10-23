ad76fa59e7517045cb2f651ee1e2bdb3ea39c0c9
Oscar Wiersdalen Thunold <79252382+Oggyboggi@users.noreply.github.com>
2025-10-23 18:19:41 +0200

    Update drawing.py

--- Deliverable2/drawing.py
+++ Deliverable2/drawing.py
@@ -3,6 +3,7 @@ Created by Iver Rannug Fossan and Oscar Wiersdalen Thunold
 
 Task 1
 """""
+import turtle as t
 
 # Class pulled from lecture notebook 8
 class Rectangle:
@@ -18,6 +19,20 @@ class Rectangle:
     # Method to return information about the rectangle
     def info(self):
         return f"Rectangle: lower left {self._ll}, upper right {self._ll[0] + self._width, self._ll[1] + self._height}"
-    
+
+    def draw(self, Turtle, scale=50):
+        Turtle.penup()
+        Turtle.goto(self._ll[0]*scale, self._ll[1]*scale)
+        Turtle.pendown()
+        for _ in range(2):
+            Turtle.forward(self._width*scale)
+            Turtle.left(90)
+            Turtle.forward(self._height*scale)
+            Turtle.left(90)
+
 if __name__ == "__main__":
-    print(Rectangle((1,2), (4,6)).info()) # Prints: Rectangle: lower left (1, 2), upper right (4, 6)
+    Turtle = t.Turtle()
+    rect = Rectangle((1, 2), (4, 6))
+    print(rect.info())
+    rect.draw(Turtle)
+    t.done()
