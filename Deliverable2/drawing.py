22ce94b96cbb9c322d4b4ea29e951e60ba66519a
Oscar Wiersdalen Thunold <79252382+Oggyboggi@users.noreply.github.com>
2025-10-23 15:41:42 +0200

    Add files via upload

--- Deliverable2/drawing.py
+++ Deliverable2/drawing.py
@@ -10,10 +10,14 @@ class Rectangle:
         self._ll = lower_left
         self._width = upper_right[0] - lower_left[0]
         self._height = upper_right[1] - lower_left[1]
-        
+
+    # Method to calculate area of the rectangle
     def area(self):
         return self._width * self._height
     
-
-if __name__ == "__main__":
+    # Method to return information about the rectangle
+    def info(self):
+        return f"Rectangle: lower left {self._ll}, upper right {self._ll[0] + self._width, self._ll[1] + self._height}"
     
+if __name__ == "__main__":
+    print(Rectangle((1,2), (4,6)).info()) # Prints: Rectangle: lower left (1, 2), upper right (4, 6)
