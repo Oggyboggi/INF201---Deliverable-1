3ec3cb893392a08e90f2ef38d6b7a39571262515
IverFossan <iver.rannug.fossan@nmbu.no>
2025-10-27 17:21:22 +0100

    Add files via upload

--- Deliverable2/drawing.py
+++ Deliverable2/drawing.py
@@ -5,6 +5,10 @@ Task 1
 """""
 import turtle as t
 
+# Rectangle-klassen bygger på eksempelet fra forelesning 8.
+# Kilde: Forelesningsnotat (Lecture 8).
+
+
 # Class pulled from lecture notebook 8
 class Rectangle:
     def __init__(self, lower_left, upper_right):
@@ -16,14 +20,22 @@ class Rectangle:
     def area(self):
         return self._width * self._height
     
-    # Method to return information about the rectangle
+    # skriver ut venstre nedre og høyre øvre hjørne
     def info(self):
-        return f"Rectangle: lower left {self._ll}, upper right {self._ll[0] + self._width, self._ll[1] + self._height}"
+        ur = (self._ll[0] + self._width, self._ll[1] + self._height)
+        print(f"Rectangle: lower left {self._ll}, upper right {ur}")
+    
 
     def draw(self, Turtle, scale=50):
+        
+        Turtle.pensize(3)
+        Turtle.speed(3)
+        
         Turtle.penup()
         Turtle.goto(self._ll[0]*scale, self._ll[1]*scale)
         Turtle.pendown()
+        
+
         for _ in range(2):
             Turtle.forward(self._width*scale)
             Turtle.left(90)
@@ -31,8 +43,22 @@ class Rectangle:
             Turtle.left(90)
 
 if __name__ == "__main__":
+
     Turtle = t.Turtle()
-    rect = Rectangle((1, 2), (4, 6))
-    print(rect.info())
-    rect.draw(Turtle)
+
+    # Tre forskjellige rektangler
+    rects = [
+        Rectangle((-1.5, -2), (1.5, 2)),   # sentrert, bredde=3, høyde=4
+        Rectangle((-3, -1), (0, 1)),       # litt til venstre
+        Rectangle((0.5, -0.5), (2.5, 1.5)) # litt til høyre
+    ]
+
+    for r in rects:
+        r.draw(Turtle, scale=50)
+
     t.done()
+
+    colors = ["red", "green", "orange"]
+    for r, c in zip(rects, colors):
+        Turtle.pencolor(c)
+        r.draw(Turtle, scale=50)
