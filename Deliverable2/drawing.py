5b309f80e79c9310b162593e39b9b752b44ab3af
Oscar Wiersdalen Thunold <79252382+Oggyboggi@users.noreply.github.com>
2025-10-29 00:23:06 +0100

    Update drawing.py

--- Deliverable2/drawing.py
+++ Deliverable2/drawing.py
@@ -1,21 +1,21 @@
-
 """
 Created by Iver Rannug Fossan and Oscar Wiersdalen Thunold
 
-Task 1 + start Task 2
+INF201 - Deliverable 2
 """
 import turtle as t
+import math
 
-# Rectangle-klassen bygger på eksempelet fra forelesning 8.
-# Kilde: Forelesningsnotat (Lecture 8).
-
-
+# Rectangle class template is pulled from Lecture notebook 8.
+# Down under is the modified and complete version
 class Rectangle:
-    def __init__(self, lower_left, upper_right):
+    def __init__(self, lower_left, upper_right, color="black", linewidth=1):
         self._ll = lower_left
         self._ur = upper_right
         self._width = upper_right[0] - lower_left[0]
         self._height = upper_right[1] - lower_left[1]
+        self._color = color
+        self._linewidth = linewidth
 
     def area(self):
         return self._width * self._height
@@ -27,7 +27,8 @@ class Rectangle:
         turtle_obj.penup()
         turtle_obj.goto(self._ll[0] * scale, self._ll[1] * scale)
         turtle_obj.pendown()
-        turtle_obj.pensize(3)
+        turtle_obj.pencolor(self._color)
+        turtle_obj.pensize(self._linewidth)
 
         for _ in range(2):
             turtle_obj.forward(self._width * scale)
@@ -36,44 +37,103 @@ class Rectangle:
             turtle_obj.left(90)
 
 
-# Oppgave 3: Partially done, to be completed by partner
+# Oppgave 3
 class Triangle:
-    def __init__(self, p1, p2, p3):
+    def __init__(self, p1, p2, p3, color="red", linewidth=1):
         self._p1 = p1
         self._p2 = p2
         self._p3 = p3
-        # TODO: add color and linewidth as keyword arguments
-        # TODO: implement info() that prints points, color, linewidth
-        # TODO: implement draw() using turtle
+        self._color = color
+        self._linewidth = linewidth
+
+    def area(self):
+        x1, y1 = self._p1
+        x2, y2 = self._p2
+        x3, y3 = self._p3
+        return 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+
+    def info(self):
+        print(f"Triangle: points {self._p1}, {self._p2}, {self._p3}, "
+              f"color {self._color}, linewidth {self._linewidth}, area {self.area()}")
+
+    def draw(self, turtle_obj, scale=50):
+        turtle_obj.penup()
+        turtle_obj.pencolor(self._color)
+        turtle_obj.pensize(self._linewidth)
+
+        p1s = (self._p1[0] * scale, self._p1[1] * scale)
+        p2s = (self._p2[0] * scale, self._p2[1] * scale)
+        p3s = (self._p3[0] * scale, self._p3[1] * scale)
+
+        turtle_obj.goto(*p1s); turtle_obj.pendown()
+        turtle_obj.goto(*p2s); turtle_obj.goto(*p3s); turtle_obj.goto(*p1s)
+        turtle_obj.penup()
 
 
 class Circle:
-    def __init__(self, center, radius):
+    def __init__(self, center, radius, color="blue", linewidth=1):
         self._center = center
         self._radius = radius
-        # TODO: add color and linewidth as keyword arguments
-        # TODO: implement info()
-        # TODO: draw as many-sided polygon with turtle
+        self._color = color
+        self._linewidth = linewidth
+
+    def info(self):
+        print(f"Circle: center {self._center}, radius {self._radius}, "
+              f"color {self._color}, linewidth {self._linewidth}")
+
+    def draw(self, turtle_obj, scale=50, sides=120):
+        cx, cy = self._center
+        r = self._radius
+
+        # start på (cx + r, cy)
+        start = ((cx + r) * scale, cy * scale)
+
+        turtle_obj.penup()
+        turtle_obj.pencolor(self._color)
+        turtle_obj.pensize(self._linewidth)
+        turtle_obj.goto(*start)
+        turtle_obj.setheading(0)
+        turtle_obj.pendown()
+
+        side_len = (2 * math.pi * r / sides) * scale
+        turn = 360 / sides
+        for _ in range(sides):
+            turtle_obj.forward(side_len)
+            turtle_obj.left(turn)
+        turtle_obj.penup()
 
 
 if __name__ == "__main__":
     Turtle = t.Turtle()
     Turtle.speed(0)
+    Turtle.hideturtle()
 
-    # Task 1 & Task 2
+    print("Rectangles")
     rects = [
-        Rectangle((-1.5, -2), (1.5, 2)),
-        Rectangle((-4, -1), (-1, 1)),
-        Rectangle((2, -0.5), (4.5, 1.5))
+        Rectangle((-1.5, -2), (1.5, 2), color="black", linewidth=3),
+        Rectangle((-4, -1), (-1, 1), color="gray", linewidth=2),
+        Rectangle((2, -0.5), (4.5, 1.5), color="purple", linewidth=2),
     ]
-
-    print("== Rectangles ==")
     for r in rects:
         r.info()
         r.draw(Turtle, scale=50)
 
-    # Task 3 will be completed later
-    # TODO: create list of Triangle and Circle instances
-    # TODO: call info() and draw() for each
+    print("Triangles")
+    triangles = [
+        Triangle((0, 0), (2, 0), (1, 2), color="red", linewidth=2),
+        Triangle((-3, -1), (-1, -1), (-2, 1), color="green", linewidth=3),
+    ]
+    for tri in triangles:
+        tri.info()
+        tri.draw(Turtle, scale=50)
+
+    print("Circles")
+    circles = [
+        Circle((3, 3), 1, color="blue", linewidth=2),
+        Circle((-3, 3), 0.5, color="orange", linewidth=3),
+    ]
+    for c in circles:
+        c.info()
+        c.draw(Turtle, scale=50, sides=120)
 
     t.done()
